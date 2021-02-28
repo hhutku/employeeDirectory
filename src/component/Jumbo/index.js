7d849e6a933319caadea7c58cbe8f2d360e9f73c
hhutku <hhutku.wa@gmail.com>
2021-02-28 01:20:08 -0800

    filtering function added

---
+++ src/component/Jumbo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Jumbo = () => {
+    return (
+        <div className="container">
+        <div  className="jumbotron">
+          <h1>Employee Directory</h1>                 
+        </div>         
+      </div>
+    )
+}
+
+export default Jumbo
--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -1,21 +1,31 @@
 import React, {useState,useEffect} from 'react'
 import useAPI from '../../utils/useAPI'
 import Search from '../Search'
+import Jumbo from '../Jumbo'
 
 
 const Main = () => {
 
     const [search, setSearch] = useState("");
     const [result, setResult] = useState([]);
+    const [filteredResult, setFilteredResult] = useState([]);
 
     useAPI(setResult)
 
+    useEffect(() => {
+        const filteredEmployees = result.filter(employee => employee.name.last
+            .toLowerCase().indexOf(search.toLowerCase()) !== -1)
+               setFilteredResult(filteredEmployees)
+
+    }, [search, result])
+
     console.log(result)
+    console.log(filteredResult)
   
 
     return (
         <div>
-           
+            <Jumbo />
             <Search setSearch={setSearch} />
        <h1>{search}</h1>
             
